37152a192faca4153f8020cd9ae4b7ffb2178611
Jason Nall <jsonnull@gmail.com>
2017-10-11 21:40:51 -0400

    Start local server in CI environment.

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,13 +1,16 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 
-const filepath = path.resolve(__dirname, '../public/twitter.html')
-;(async () => {
+const main = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.setViewport({ width: 1500, height: 421 })
-  await page.goto('file://' + filepath, { waitUntil: 'networkidle' })
+  await page.goto('http://localhost:8080/twitter.html', {
+    waitUntil: 'networkidle'
+  })
   await page.screenshot({ path: 'public/twitterHeader.png' })
 
   await browser.close()
-})()
+}
+
+main()
