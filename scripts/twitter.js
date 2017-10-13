11a34a045961743c22301f6e2e33a7b6a05b940e
Jason Nall <jsonnull@gmail.com>
2017-10-12 20:03:47 -0400

    Add error catcher to twitter script.

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -5,10 +5,14 @@ const main = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.setViewport({ width: 1500, height: 500 })
-  await page.goto('http://localhost:8080/twitter.html', {
-    waitUntil: 'networkidle'
-  })
-  await page.screenshot({ path: 'public/twitterHeader.png' })
+  try {
+    await page.goto('http://localhost:8080/twitter.html', {
+      waitUntil: 'networkidle'
+    })
+    await page.screenshot({ path: 'public/twitterHeader.png' })
+  } catch (e) {
+    console.error(e)
+  }
 
   await browser.close()
 }
