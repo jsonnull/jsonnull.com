61258f1b05683dfbc6807b632018687bb626b1fc
Jason Nall <jsonnull@gmail.com>
2017-10-15 18:48:29 -0400

    Update server script to work on CI and production environments.

--- src/server/twitter.js
+++ src/server/twitter.js
@@ -1,12 +1,19 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 
+const URL =
+  process.env.TRAVIS == true
+    ? 'http://localhost:8080/twitter.html'
+    : 'https://jsonnull.com/twitter.html'
+
 const main = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
   const page = await browser.newPage()
   await page.setViewport({ width: 1500, height: 500 })
   try {
-    await page.goto('http://localhost:8080/twitter.html', {
+    await page.goto(URL, {
       waitUntil: 'networkidle'
     })
     await page.screenshot({ path: 'public/twitterHeader.png' })
