90cc2dde8970127d9b6089ab2b276aee2e2c13fa
Jason Nall <jsonnull@gmail.com>
2017-10-16 16:23:35 -0400

    Make over projects section.

--- src/client/components/home/Projects.js
+++ src/client/components/home/Projects.js
@@ -10,12 +10,23 @@ const Inner = styled.div`
   padding: 7.2rem 0;
   ${media.mobile`
     padding: 7.2rem 0;
-  `} ${media.tablet`
+  `};
+  ${media.tablet`
     padding: 14.4rem 0;
     margin: 0 auto;
     width: 700px;
-  `} ${media.desktopHuge`
+  `};
+  ${media.desktopHuge`
     width: 1000px;
+  `};
+`
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  ${media.desktop`
+    flex-direction: row;
   `};
 `
 
@@ -101,6 +112,7 @@ const Title = styled.h3`
   font-weight: 700;
   color: ${colors.gray};
   margin: 0;
+  margin-bottom: 1em;
 
   ${media.mobile`
     font-size: ${fontSize.large};
@@ -108,56 +120,62 @@ const Title = styled.h3`
 `
 
 const Paragraph = styled.p`
-  margin: 3.6rem 0;
+  margin: 0;
+  margin-bottom: 2em;
   font-size: ${fontSize.small};
   line-height: ${lineUnit};
+  width: 100%;
   ${media.tablet`
-    font-size: ${fontSize.normal};
-    line-height: 3rem;
     width: 75%;
+    font-size: ${fontSize.normal};
+  `};
+  ${media.desktop`
+    width: 33%;
+    padding-right: 2.4rem;
   `};
 `
 
 const Projects = styled.div`
-  display: flex;
-  flex-direction: column;
+  width: 100%;
 
-  ${media.tablet`
-    flex-direction: row;
+  ${media.desktop`
+    width: 66%;
   `};
 `
 
 const ProjectSection = () => (
   <Recent>
     <Inner>
-      <Title>I {'<3'} open source.</Title>
-      <Paragraph>
-        I love consuming and contributing to open source. Side projects are a
-        regular opportunity to learn and try new things. Here are some I've been
-        involved with recently.
-      </Paragraph>
-      <Projects>
-        <Tile
-          title="Aleamancer"
-          url="https://www.aleamancer.com"
-          address="www.aleamancer.com"
-          tags={['Design', 'Development', 'JavaScript', 'Open Source']}
-          color={colors.red}
-        >
-          A groundbreaking online tabletop role-playing platform, built with
-          cutting-edge tools and tech from the React+Redux ecosystem.
-        </Tile>
-        <Tile
-          title="jsonnull"
-          url="https://github.com/jsonnull/jsonnull.com"
-          address="github.com/jsonnull/jsonnull.com"
-          tags={['Design', 'Development', 'JavaScript']}
-          color={colors.orange}
-        >
-          The content and build system for this site, a statically rendered
-          React application.
-        </Tile>
-      </Projects>
+      <Title>I {'<3'} open source</Title>
+      <Section>
+        <Paragraph>
+          I love consuming and contributing to open source. Side projects are a
+          regular opportunity to learn and try new things. Here are some I've
+          been involved with recently.
+        </Paragraph>
+        <Projects>
+          <Tile
+            title="Aleamancer"
+            url="https://www.aleamancer.com"
+            address="www.aleamancer.com"
+            tags={['Design', 'Development', 'JavaScript', 'Open Source']}
+            color={colors.red}
+          >
+            A groundbreaking online tabletop role-playing platform, built with
+            cutting-edge tools and tech from the React+Redux ecosystem.
+          </Tile>
+          <Tile
+            title="jsonnull"
+            url="https://github.com/jsonnull/jsonnull.com"
+            address="github.com/jsonnull/jsonnull.com"
+            tags={['Design', 'Development', 'JavaScript']}
+            color={colors.orange}
+          >
+            The content and build system for this site, a statically rendered
+            React application.
+          </Tile>
+        </Projects>
+      </Section>
     </Inner>
   </Recent>
 )
