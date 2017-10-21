484e6968bac1eacc2b8179d8d16e2b0e0f5f20c4
Jason Nall <jsonnull@gmail.com>
2017-10-21 16:43:07 -0400

    Make screenshot script generic.

--- src/server/scripts/screenshots.js
+++ src/server/scripts/screenshots.js
@@ -1,24 +1,56 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 
-const URL =
+const HOST =
   process.env.TRAVIS == true
-    ? 'http://localhost:8080/twitter.html'
-    : 'https://jsonnull.com/twitter.html'
+    ? 'http://localhost:8080/'
+    : 'https://jsonnull.com/'
+
+const readme = {
+  url: '',
+  size: {
+    width: 1920,
+    height: 1080
+  },
+  options: {
+    path: 'public/readme.jpeg',
+    quality: 70
+  }
+}
+
+const twitter = {
+  url: 'twitter.html',
+  size: {
+    width: 1500,
+    height: 500
+  },
+  options: {
+    path: 'public/twitterHeader.png'
+  }
+}
+
+const screenshots = [twitter]
 
 const main = async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
+
   const page = await browser.newPage()
-  await page.setViewport({ width: 1500, height: 500 })
-  try {
-    await page.goto(URL, {
-      waitUntil: 'networkidle'
-    })
-    await page.screenshot({ path: 'public/twitterHeader.png' })
-  } catch (e) {
-    console.error(e)
+
+  for (let i = 0; i < screenshots.length; i++) {
+    let screenshot = screenshots[i]
+    if (screenshot.size) {
+      await page.setViewport(screenshot.size)
+    }
+    try {
+      await page.goto(HOST + screenshot.url, {
+        waitUntil: 'networkidle'
+      })
+      await page.screenshot(screenshot.options)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   await browser.close()
