f269b53c1dee2ef683a93ab56a85f2beae0ce42e
Jason Nall <jsonnull@gmail.com>
2018-01-25 20:59:55 -0500

    Fix React Developer Tools warning. :)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -46,7 +46,38 @@ const browser = {
     new webpack.optimize.ModuleConcatenationPlugin(),
     new UglifyJsPlugin({
       uglifyOptions: {
-        compress: false,
+        compress: {
+          arrows: false,
+          booleans: false,
+          collapse_vars: false,
+          comparisons: false,
+          computed_props: false,
+          hoist_funs: false,
+          hoist_props: false,
+          hoist_vars: false,
+          if_return: false,
+          inline: false,
+          join_vars: false,
+          keep_infinity: true,
+          loops: false,
+          negate_iife: false,
+          properties: false,
+          reduce_funcs: false,
+          reduce_vars: false,
+          sequences: false,
+          side_effects: false,
+          switches: false,
+          top_retain: false,
+          toplevel: false,
+          typeofs: false,
+          unused: false,
+
+          // Switch off all types of compression except those needed to convince
+          // react-devtools that we're using a production build
+          conditionals: true,
+          dead_code: true,
+          evaluate: true
+        },
         mangle: true,
         parallel: true
       }
