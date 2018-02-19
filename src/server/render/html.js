3316d0c6d93765736689dac00f2fcfe6c6bfbbdd
Jason Nall <jsonnull@gmail.com>
2018-02-19 18:47:10 -0500

    Reenable production analytics script.

--- src/server/render/html.js
+++ src/server/render/html.js
@@ -44,7 +44,7 @@ module.exports = function createHtml(props) {
         <script>
           window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
         </script>
-        <script async src='https://www.google-analytics.com/analytics_debug.js'></script>
+        <script async src='https://www.google-analytics.com/analytics.js'></script>
       </head>
       <body>
         <div id="react-root">${body}</div>
