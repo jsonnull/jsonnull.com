9ef580ab8e6f2d581c3d01b989e14f266c726399
Jason Nall <jsonnull@gmail.com>
2018-02-19 18:41:07 -0500

    Temporarily enable debug analytics script.

--- src/server/render/html.js
+++ src/server/render/html.js
@@ -43,10 +43,8 @@ module.exports = function createHtml(props) {
 
         <script>
           window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'UA-112190148-1', 'auto');
-          ga('set', 'transport', 'beacon');
         </script>
-        <script async src='https://www.google-analytics.com/analytics.js'></script>
+        <script async src='https://www.google-analytics.com/analytics_debug.js'></script>
       </head>
       <body>
         <div id="react-root">${body}</div>
