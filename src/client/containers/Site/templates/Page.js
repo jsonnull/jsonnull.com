c3c78f54de45814c37676d87ef553aa9c6792fcb
Jason Nall <jsonnull@gmail.com>
2018-03-21 18:03:14 -0400

    Add import pipeline for standalone page content.

--- src/client/containers/Site/templates/Page.js
+++ src/client/containers/Site/templates/Page.js
@@ -1,21 +1,54 @@
+// @flow
 import React from 'react'
 import styled from 'styled-components'
+import { withRouter } from 'react-router'
 import Wrapper from '../../../components/Wrapper'
 import Section from '../../../components/Section'
 import Content from '../../../components/Content'
 import Title from '../../../components/navigation/Title'
 import Links from '../../../components/navigation/Links'
 import Footer from '../../../components/navigation/Footer'
+import { asyncComponent } from 'react-async-component'
+import type { RenderProps } from '../../../types'
 
-const Page = props => (
-  <Wrapper>
-    <Section>
-      <Content {...props} />
-    </Section>
-    <Title />
-    <Links />
-    <Footer />
-  </Wrapper>
-)
+type Props = {
+  match: Object
+} & RenderProps
 
-export default Page
+const Page = (props: Props) => {
+  let pathname = props.location.pathname
+  if (pathname.indexOf('.html') == -1) {
+    pathname = pathname + 'index.html'
+  }
+
+  pathname = pathname.replace('.html', '.md')
+
+  const Body = asyncComponent({
+    resolve: () =>
+      new Promise(async (resolve, reject) => {
+        const {
+          content,
+          data
+        } = await import(`../../../../../content${pathname}`)
+
+        const component = () => (
+          <Section>
+            <Content content={content} />
+          </Section>
+        )
+
+        resolve(component)
+      })
+  })
+
+  return (
+    <Wrapper>
+      <Body />
+      <Title />
+      <Links />
+      <Footer />
+    </Wrapper>
+  )
+}
+
+export default withRouter(Page)
