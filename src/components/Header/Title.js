99b88b364c63532f4e10714b6d1365e5373e8d5b
Jason Nall <jsonnull@gmail.com>
2018-11-09 22:36:41 -0800

    Update hero design and message.

--- src/components/Header/Title.js
+++ src/components/Header/Title.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react'
 import styled from 'react-emotion'
+import map from 'styled-map'
 import { Link } from 'gatsby'
 import { colors, fonts, fontSize, lineUnit, media } from '../../styles/base'
 import { color, underlineIn, underlineOut } from '../../styles/links'
@@ -11,7 +12,11 @@ const Title = styled.h1`
   line-height: ${lineUnit};
   height: ${lineUnit};
   z-index: 700;
-  color: ${colors.white};
+
+  font-weight: ${map`
+    inverted: 400;
+    default: 700;
+  `};
 
   ${media.mobile`
     font-size: ${fontSize.large};
@@ -24,7 +29,11 @@ const Title = styled.h1`
   a:active {
     transition: background-size 100ms ease;
     text-shadow: none;
-    ${color(colors.white)} ${underlineOut} background-position: 0 100%;
+    ${props =>
+      props.inverted
+        ? color(colors.white)
+        : color(colors.black)} ${underlineOut};
+    background-position: 0 100%;
   }
 
   a:hover {
@@ -33,8 +42,8 @@ const Title = styled.h1`
   }
 `
 
-const TitleEl = () => (
-  <Title>
+const TitleEl = ({ inverted }) => (
+  <Title inverted={inverted}>
     <Link to="/">Jason Nall</Link>
   </Title>
 )
