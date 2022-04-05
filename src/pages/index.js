c58cf1532b019d88ae3535e8be6e118a7b6ea78d
Jason Nall <jsonnull@gmail.com>
2022-04-04 22:20:34 -0400

    Better mobile navigation.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import profilePic from '../../static/img/photo.jpg'
 
 const Title = () => {
   return (
-    <div className="flex flex-col mx-8 py-32 items-center">
+    <div className="flex flex-col mx-8 py-32 items-start sm:items-center">
       <Image
         src={profilePic}
         alt="Picture of the author"
@@ -14,7 +14,7 @@ const Title = () => {
         placeholder="blur" // Optional blur-up while loading
         className="rounded-full"
       />
-      <h1 className="pt-4 text-5xl font-semibold tracking-tight">
+      <h1 className="pt-4 text-3xl sm:text-5xl font-semibold tracking-tight">
         Crafting{' '}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-fog to-steel">
           experiences
