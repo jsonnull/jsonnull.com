95c610bd29541d5f3e59fceee4dcfca4c1f39692
Jason Nall <jsonnull@gmail.com>
2022-04-04 22:25:27 -0400

    Better mobile navigation (#54)

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import clsx from 'clsx'
 
 const Separator = ({ className }) => {
   return (
-    <div className={clsx(className, 'px-6')}>
+    <div className={clsx(className, 'px-3 sm:px-6')}>
       <div className="border-l w-0 border-solid border-zinc-500">&nbsp;</div>
     </div>
   )
@@ -13,15 +13,15 @@ const Separator = ({ className }) => {
 
 export const Header = () => {
   return (
-    <div className="fixed bottom-8 inset-x-8 flex pointer-events-none">
-      <nav className="mx-auto px-6 py-4 backdrop-blur flex items-center text-white bg-black/80 rounded-md pointer-events-auto">
+    <div className="fixed bottom-2 sm:bottom-8 inset-x-2 sm:inset-x-8 flex pointer-events-none">
+      <nav className="w-full sm:w-auto sm:mx-auto pl-4 pr-2 sm:px-6 sm:pr-4 py-4 backdrop-blur flex items-center text-white bg-black/80 rounded-md pointer-events-auto">
         <Link href="/">
           <a className="hover:underline decoration-zinc-400">
             <span className="font-bold">jsonnull</span>
           </a>
         </Link>
         <Separator />
-        <ul className="flex gap-6">
+        <ul className="flex gap-3 sm:gap-6">
           {[
             ['/projects/', 'projects'],
             // ['/blog/', 'blog'],
@@ -36,8 +36,8 @@ export const Header = () => {
             )
           })}
         </ul>
-        <Separator className="pr-4" />
-        <ul className="flex gap-2 leading-none items-center">
+        <Separator className="pr-1 sm:pr-4 hidden sm:block" />
+        <ul className="flex gap-1 sm:gap-2 ml-auto leading-none items-center">
           {[
             [GitHub, 'https://github.com/jsonnull'],
             [Twitter, 'https://twitter.com/jsonnull'],
