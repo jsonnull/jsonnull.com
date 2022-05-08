61bf29d25f5b02cb76f6528b39b5418d7822dfa6
Jason Nall <jsonnull@gmail.com>
2022-05-08 14:07:29 -0400

    Ensure lack of netlify URL is handled gracefully.

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,16 +3,21 @@ import { Header } from './'
 import { Spacer } from './Spacer.tsx'
 
 export const Page = ({ children, title = 'Welcome', ogImage }) => {
-  const ogImageURL = new URL(
-    ogImage,
-    process.env.NEXT_PUBLIC_NETLIFY_URL ?? process.env.NEXT_PUBLIC_SITE_URL
-  )
+  let ogImageURL
+  if (ogImage) {
+    let baseUrl =
+      process.env.NEXT_PUBLIC_NETLIFY_URL &&
+      process.env.NEXT_PUBLIC_NETLIFY_URL !== ''
+        ? process.env.NEXT_PUBLIC_NETLIFY_URL
+        : process.env.NEXT_PUBLIC_SITE_URL
+    ogImageURL = new URL(ogImage, baseUrl)
+  }
   return (
     <div className="flex flex-col bg-white dark:bg-black text-black dark:text-zinc-200 min-h-screen">
       <Head>
         <title>{title} | Jason Nall</title>
-        {ogImage && <meta property="og:image" content={ogImageURL} />}
-        {ogImage && <meta name="twitter:image" content={ogImageURL} />}
+        {ogImageURL && <meta property="og:image" content={ogImageURL} />}
+        {ogImageURL && <meta name="twitter:image" content={ogImageURL} />}
       </Head>
       <Header />
       <div className="flex flex-col">{children}</div>
