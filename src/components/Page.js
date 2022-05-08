9bbdf3058b6395458e1db7faee6d2a0caaea1929
Jason Nall <jsonnull@gmail.com>
2022-05-08 02:29:04 -0400

    Update meta tags.

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,19 +2,18 @@ import { Helmet } from 'react-helmet'
 import { Header } from './'
 import { Spacer } from './Spacer.tsx'
 
-export const Page = ({ children, title = 'Welcome', ogImage }) => (
-  <div className="flex flex-col bg-white dark:bg-black text-black dark:text-zinc-200 min-h-screen">
-    <Helmet titleTemplate="%s | Jason Nall">
-      <title>{title}</title>
-      {ogImage && (
-        <>
-          <meta property="og:image" content={ogImage} />
-          <meta name="twitter:image" content={ogImage} />
-        </>
-      )}
-    </Helmet>
-    <Header />
-    <div className="flex flex-col">{children}</div>
-    <Spacer extraWide />
-  </div>
-)
+export const Page = ({ children, title = 'Welcome', ogImage }) => {
+  console.log(ogImage)
+  return (
+    <div className="flex flex-col bg-white dark:bg-black text-black dark:text-zinc-200 min-h-screen">
+      <Helmet titleTemplate="%s | Jason Nall">
+        <title>{title}</title>
+        {ogImage && <meta property="og:image" content={ogImage} />}
+        {ogImage && <meta name="twitter:image" content={ogImage} />}
+      </Helmet>
+      <Header />
+      <div className="flex flex-col">{children}</div>
+      <Spacer extraWide />
+    </div>
+  )
+}
