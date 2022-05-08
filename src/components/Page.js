9bc1b6b39bd974d1e9f3e23f79ebfa17e0590a27
Jason Nall <jsonnull@gmail.com>
2022-05-08 13:58:33 -0400

    Change netlify env propagation.

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,7 +5,7 @@ import { Spacer } from './Spacer.tsx'
 export const Page = ({ children, title = 'Welcome', ogImage }) => {
   const ogImageURL = new URL(
     ogImage,
-    process.env.DEPLOY_PRIME_URL ?? process.env.NEXT_PUBLIC_SITE_URL
+    process.env.NEXT_PUBLIC_NETLIFY_URL ?? process.env.NEXT_PUBLIC_SITE_URL
   )
   return (
     <div className="flex flex-col bg-white dark:bg-black text-black dark:text-zinc-200 min-h-screen">
