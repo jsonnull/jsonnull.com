498b64dbf6a3422a776ae51d347f2d7363d6f51d
Jason Nall <jsonnull@gmail.com>
2022-05-08 13:51:28 -0400

    Change og-image URL generation.

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,7 +3,10 @@ import { Header } from './'
 import { Spacer } from './Spacer.tsx'
 
 export const Page = ({ children, title = 'Welcome', ogImage }) => {
-  const ogImageURL = new URL(ogImage, process.env.NEXT_PUBLIC_SITE_URL)
+  const ogImageURL = new URL(
+    ogImage,
+    process.env.DEPLOY_PRIME_URL ?? process.env.NEXT_PUBLIC_SITE_URL
+  )
   return (
     <div className="flex flex-col bg-white dark:bg-black text-black dark:text-zinc-200 min-h-screen">
       <Head>
