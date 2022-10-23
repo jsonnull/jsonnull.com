f87acc5d94180166742b40dcb724ae52c675b578
Jason Nall <jsonnull@gmail.com>
2022-10-23 14:21:35 -0400

    Use pnpm. (#62)

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2d6ab6c6b92f7aaf8bc53baba9754b9bfdce56f2.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/30f6859a3ab126d943c23f92d2b158fe850f78f9.tar.gz") {} }:
 
 with pkgs;
 
 mkShell {
   buildInputs = [
     git
-    nodejs-16_x
-    nodePackages.yarn
+    nodejs-18_x
+    nodePackages.pnpm
   ];
 
   NEXT_PUBLIC_SITE_URL = "http://localhost:3000";
