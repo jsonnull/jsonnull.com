1743d843ad6b931c892fcd19825b9c1106ee1879
Jason Nall <jsonnull@gmail.com>
2022-10-23 19:12:49 -0400

    Remove OpenGraph test page. (#65)

--- src/pages/_meta/OpenGraph.js
+++
@@ -1,9 +0,0 @@
-import { OpenGraph } from '../../components/OpenGraph'
-
-export default () => {
-  return <div className="h-screen w-screen flex bg-zinc-800">
-    <div className="m-auto outline outline-[10px] outline-red-500">
-      <OpenGraph title={"A Blog Post Title"}/>
-    </div>
-  </div>
-}
